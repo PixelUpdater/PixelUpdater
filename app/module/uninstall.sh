efa4a9ec11b6e18f3485d27c1f5790a4bc38e716
capntrips <31455058+capntrips@users.noreply.github.com>
2024-04-03 00:50:34 -0500

    make uninstall more specific

--- app/module/uninstall.sh
+++ app/module/uninstall.sh
@@ -1,19 +1,13 @@
 APP_PACKAGE="com.github.pixelupdater.pixelupdater"
 APP_UID=$(grep $APP_PACKAGE /data/system/packages.list | cut -d " " -f 2)
 APP_DATA_DIR=$(dumpsys package $APP_PACKAGE | grep -E 'dataDir=' | cut -d'=' -f2)
+APP_EXTERNAL_DATA_DIR="/storage/emulated/0/Android/data/${APP_PACKAGE}"
 
-if [ -d "/storage/emulated/0/Android/data/$APP_PACKAGE" ]; then
-    EXTERNAL_DATA_DIR="/storage/emulated/0/Android/data/"
-elif [ -d "/data/media/0/Android/data/$APP_PACKAGE" ]; then
-    EXTERNAL_DATA_DIR="/data/media/0/Android/data/"
-fi
-APP_EXTERNAL_DATA_DIR="${EXTERNAL_DATA_DIR}${APP_PACKAGE}"
-
-find /data/dalvik-cache -iname '*pixelupdater*' | xargs rm -f
-find /data/system/package_cache -iname '*pixelupdater*' | xargs rm -f
+find /data/dalvik-cache -name 'system@priv-app@PixelUpdater@PixelUpdater.apk@*' | xargs rm -f
+find /data/system/package_cache -name 'PixelUpdater-*' | xargs rm -f
 
 if [ -n "$APP_UID" ]; then
-    find /data/ota_package -user $APP_UID -exec rm -rf {} \;
+    find /data/ota_package -user $APP_UID | xargs rm -f
 fi
 
 if [ -n "$APP_DATA_DIR" ]; then
